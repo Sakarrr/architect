aced76b4995d5a7bbef56e968e7eaedf8135088f
sakarrr <ctha.sakar@gmail.com>
2024-08-27 11:13:56 +0545

    Add - Projects section

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
 
 <body>
     <div class="arc-header">
-        <div class="arc-container">
+        <div class="arc-container-fluid">
             <div class="arc-row">
                 <div class="arc-branding"> <a href="#"><b>BR</b> Architects</a></div>
                 <div class="arc-menu">
@@ -38,7 +38,114 @@
         </div>
     </div>
 
-    <script src="./assets/js/jquery.js"></script>
+    <div class="arc-projects">
+        <div class="arc-container">
+            <div class="arc-section-header">
+                <h2 class="section-heading">Projects</h2>
+            </div>
+
+            <div class="arc-projects-lists">
+                <div class="arc-row">
+                    <div class="arc-project arc-col arc-col-sm-6 arc-col-md-3">
+                        <div class="arc-project-tag">Summer House</div>
+                        <img src="https://www.w3schools.com/w3images/house5.jpg">
+                    </div>
+                    <div class="arc-project arc-col arc-col-sm-6 arc-col-md-3">
+                        <div class="arc-project-tag">Brick</div>
+                        <img src="https://www.w3schools.com/w3images/house2.jpg">
+                    </div>
+                    <div class="arc-project arc-col arc-col-sm-6 arc-col-md-3">
+                        <div class="arc-project-tag">Renovated</div>
+                        <img src="https://www.w3schools.com/w3images/house3.jpg">
+                    </div>
+                    <div class="arc-project arc-col arc-col-sm-6 arc-col-md-3">
+                        <div class="arc-project-tag">Barn House</div>
+                        <img src="https://www.w3schools.com/w3images/house4.jpg">
+                    </div>
+                    <div class="arc-project arc-col arc-col-sm-6 arc-col-md-3">
+                        <div class="arc-project-tag">Summer House</div>
+                        <img src="https://www.w3schools.com/w3images/house2.jpg">
+                    </div>
+                    <div class="arc-project arc-col arc-col-sm-6 arc-col-md-3">
+                        <div class="arc-project-tag">Brick</div>
+                        <img src="https://www.w3schools.com/w3images/house5.jpg">
+                    </div>
+                    <div class="arc-project arc-col arc-col-sm-6 arc-col-md-3">
+                        <div class="arc-project-tag">Renovated</div>
+                        <img src="https://www.w3schools.com/w3images/house4.jpg">
+                    </div>
+                    <div class="arc-project arc-col arc-col-sm-6 arc-col-md-3">
+                        <div class="arc-project-tag">Barn House</div>
+                        <img src="https://www.w3schools.com/w3images/house3.jpg">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="arc-about">
+        <div class="arc-container">
+            <div class="arc-section-header">
+                <h2 class="section-heading">About</h2>
+            </div>
+            <div class="section-description">
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
+                    et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
+                    ex ea commodo consequat. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+                    mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+                    magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                    consequat.
+                </p>
+            </div>
+
+            <div class="arc-designers-list">
+                <div class="arc-row">
+                    <div class="arc-designer-card col col-sm-6 col-md-3">
+                        <img src="https://www.w3schools.com/w3images/team2.jpg">
+                        <div class="arc-card-content">
+                            <h3 class="arc-fs-xl">John Doe</h3>
+                            <span>CEO & Founder</span>
+                            <p>
+                                Phasellus eget enim eu lectus faucibus vestibulum. Suspendisse sodales pellentesque elementum.
+                            </p>
+                        </div>
+                    </div>
+                    <div class="arc-designer-card col col-sm-6 col-md-3">
+                        <img src="https://www.w3schools.com/w3images/team1.jpg">
+                        <div class="arc-card-content">
+                            <h3 class="arc-fs-xl">Jane Doe</h3>
+                            <span>Designer</span>
+                            <p>
+                                Phasellus eget enim eu lectus faucibus vestibulum. Suspendisse sodales pellentesque elementum.
+                            </p>
+                        </div>
+                    </div>
+                    <div class="arc-designer-card col col-sm-6 col-md-3">
+                        <img src="https://www.w3schools.com/w3images/team3.jpg">
+                        <div class="arc-card-content">
+                            <h3 class="arc-fs-xl">Mike Ross</h3>
+                            <span>Architect</span>
+                            <p>
+                                Phasellus eget enim eu lectus faucibus vestibulum. Suspendisse sodales pellentesque elementum.
+                            </p>
+                        </div>
+                    </div>
+                    <div class="arc-designer-card col col-sm-6 col-md-3">
+                        <img src="https://www.w3schools.com/w3images/team4.jpg">
+                        <div class="arc-card-content">
+                            <h3 class="arc-fs-xl">Dan Star</h3>
+                            <span>Architect</span>
+                            <p>
+                                Phasellus eget enim eu lectus faucibus vestibulum. Suspendisse sodales pellentesque elementum.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <script src="./assets/js/jquery.js"></script>
 </body>
 
 </html>
