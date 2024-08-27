854e2c93a08997343658fb1ac0d00e17369b524d
sakarrr <ctha.sakar@gmail.com>
2024-08-27 10:33:05 +0545

    Add - Header and hero section

--- index.php
+++ index.php
@@ -11,7 +11,31 @@
 
 <body>
     <div class="arc-header">
-        <div class="row"></div>
+        <div class="arc-container">
+            <div class="arc-row">
+                <div class="arc-branding"> <a href="#"><b>BR</b> Architects</a></div>
+                <div class="arc-menu">
+                    <ul class="arc-row">
+                        <li><a href="#projects">Projects</a></li>
+                        <li><a href="#about">About</a></li>
+                        <li><a href="#contact">Contact</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="arc-hero">
+        <div class="arc-hero-image">
+            <img src="https://www.w3schools.com/w3images/architect.jpg">
+        </div>
+
+        <div class="arc-hero-text">
+            <h1>
+                <span class="hero-text-large">BR</span>
+                <span class="hero-text-small">Architects</span>
+            </h1>
+        </div>
     </div>
 
     <script src="./assets/js/jquery.js"></script>
