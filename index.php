4058a46e7b8c3231a9994c8150290ac006c0a1a0
sakarrr <ctha.sakar@gmail.com>
2024-08-28 14:49:38 +0545

    Refactor - Padding classes

--- index.php
+++ index.php
@@ -43,15 +43,15 @@
     <!-- Projects Section -->
     <div class="arc-projects" id="arc-projects">
         <div class="arc-container">
-            <div class="arc-section-header">
+            <div class="arc-section-header arc-pt-8">
                 <h2 class="section-heading arc-fs-xl">Projects</h2>
             </div>
 
-            <div class="arc-projects-lists">
+            <div class="arc-projects-lists arc-pt-8">
                 <div class="arc-row">
                     <?php for ($i = 0; $i < 8; $i++) { ?>
                         <div class="arc-col arc-col-sm-6 arc-col-md-3">
-                            <div class='arc-project'>
+                            <div class='arc-project arc-mt-4'>
                                 <div class="arc-project-tag">Summer House</div>
                                 <div class="arc-image-section">
                                     <img src="https://images.pexels.com/photos/106399/pexels-photo-106399.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" class='arc-img-cover'>
@@ -95,10 +95,10 @@
     <!-- About Section -->
     <div class="arc-about" id="arc-about">
         <div class="arc-container">
-            <div class="arc-section-header">
+            <div class="arc-section-header arc-pt-8">
                 <h2 class="section-heading arc-fs-xl">About</h2>
 
-                <div class="section-description">
+                <div class="section-description arc-pt-4">
                     <p>
                         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
                         et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
@@ -110,50 +110,50 @@
                 </div>
             </div>
 
-            <div class="arc-designers-list">
+            <div class="arc-designers-list arc-pt-8">
                 <div class="arc-row">
-                    <div class="arc-designer-card arc-col arc-col-sm-6 arc-col-md-3">
-                        <img class="arc-pb-2" src="https://www.w3schools.com/w3images/team2.jpg">
+                    <div class="arc-designer-card arc-col arc-col-sm-6 arc-col-md-3 arc-pb-4">
+                        <img src="https://www.w3schools.com/w3images/team2.jpg">
                         <div class="arc-card-content">
-                            <h3 class="arc-fs-xl arc-pb-3">John Doe</h3>
-                            <span class="arc-pb-3">CEO & Founder</span>
-                            <p class="arc-pb-3">
+                            <h3 class="arc-fs-xl arc-pt-2">John Doe</h3>
+                            <span class="arc-pt-3">CEO & Founder</span>
+                            <p class="arc-pt-3">
                                 Phasellus eget enim eu lectus faucibus vestibulum. Suspendisse sodales pellentesque elementum.
                             </p>
-                            <button class="arc-btn-primary arc-btn-fluid">Contact</button>
+                            <button class="arc-btn-primary arc-btn-fluid arc-mt-3">Contact</button>
                         </div>
                     </div>
-                    <div class="arc-designer-card arc-col arc-col-sm-6 arc-col-md-3">
-                        <img class="arc-pb-2" src="https://www.w3schools.com/w3images/team1.jpg">
+                    <div class="arc-designer-card arc-col arc-col-sm-6 arc-col-md-3 arc-pb-4">
+                        <img src="https://www.w3schools.com/w3images/team1.jpg">
                         <div class="arc-card-content">
-                            <h3 class="arc-fs-xl arc-pb-3">Jane Doe</h3>
-                            <span class="arc-pb-3">Designer</span>
-                            <p class="arc-pb-3">
+                            <h3 class="arc-fs-xl arc-pt-2">Jane Doe</h3>
+                            <span class="arc-pt-3">Designer</span>
+                            <p class="arc-pt-3">
                                 Phasellus eget enim eu lectus faucibus vestibulum. Suspendisse sodales pellentesque elementum.
                             </p>
-                            <button class="arc-btn-primary arc-btn-fluid">Contact</button>
+                            <button class="arc-btn-primary arc-btn-fluid arc-mt-3">Contact</button>
                         </div>
                     </div>
-                    <div class="arc-designer-card arc-col arc-col-sm-6 arc-col-md-3">
-                        <img class="arc-pb-2" src="https://www.w3schools.com/w3images/team3.jpg">
+                    <div class="arc-designer-card arc-col arc-col-sm-6 arc-col-md-3 arc-pb-4">
+                        <img src="https://www.w3schools.com/w3images/team3.jpg">
                         <div class="arc-card-content">
-                            <h3 class="arc-fs-xl arc-pb-3">Mike Ross</h3>
-                            <span class="arc-pb-3">Architect</span>
-                            <p class="arc-pb-3">
+                            <h3 class="arc-fs-xl arc-pt-2">Mike Ross</h3>
+                            <span class="arc-pt-3">Architect</span>
+                            <p class="arc-pt-3">
                                 Phasellus eget enim eu lectus faucibus vestibulum. Suspendisse sodales pellentesque elementum.
                             </p>
-                            <button class="arc-btn-primary arc-btn-fluid">Contact</button>
+                            <button class="arc-btn-primary arc-btn-fluid arc-mt-3">Contact</button>
                         </div>
                     </div>
-                    <div class="arc-designer-card arc-col arc-col-sm-6 arc-col-md-3">
-                        <img class="arc-pb-2" src="https://www.w3schools.com/w3images/team4.jpg">
+                    <div class="arc-designer-card arc-col arc-col-sm-6 arc-col-md-3 arc-pb-4">
+                        <img src="https://www.w3schools.com/w3images/team4.jpg">
                         <div class="arc-card-content">
-                            <h3 class="arc-fs-xl arc-pb-3">Dan Star</h3>
-                            <span class="arc-pb-3">Architect</span>
-                            <p class="arc-pb-3">
+                            <h3 class="arc-fs-xl arc-pt-2">Dan Star</h3>
+                            <span class="arc-pt-3">Architect</span>
+                            <p class="arc-pt-3">
                                 Phasellus eget enim eu lectus faucibus vestibulum. Suspendisse sodales pellentesque elementum.
                             </p>
-                            <button class="arc-btn-primary arc-btn-fluid">Contact</button>
+                            <button class="arc-btn-primary arc-btn-fluid arc-mt-3">Contact</button>
                         </div>
                     </div>
                 </div>
@@ -164,23 +164,23 @@
     <!-- Contact Section -->
     <div class="arc-contact" id="arc-contact">
         <div class="arc-container">
-            <div class="arc-section-header arc-pb-0">
+            <div class="arc-section-header arc-pt-8 arc-pb-0">
                 <h2 class="section-heading arc-fs-xl">Contact</h2>
 
-                <div class="section-description">
+                <div class="section-description arc-pt-4">
                     <p>
                         Lets get in touch and talk about your next project.
                     </p>
                 </div>
             </div>
 
-            <div class="arc-form">
+            <div class="arc-form arc-pt-4">
                 <form class="arc-contact-form">
-                    <input type="text" placeholder="Name">
-                    <input type="text" placeholder="Email">
-                    <input type="text" placeholder="Subject">
-                    <textarea cols="2" rows="2" placeholder="Comment"></textarea>
-                    <button class="arc-btn-primary arc-btn-bg-black">Send Message</button>
+                    <input class="arc-mt-2 arc-p-2" type="text" placeholder="Name">
+                    <input class="arc-mt-2 arc-p-2" type="text" placeholder="Email">
+                    <input class="arc-mt-2 arc-p-2" type="text" placeholder="Subject">
+                    <textarea class="arc-mt-2 arc-p-2" cols="2" rows="2" placeholder="Comment"></textarea>
+                    <button class="arc-btn-primary arc-btn-bg-black arc-mt-2">Send Message</button>
                 </form>
             </div>
         </div>
