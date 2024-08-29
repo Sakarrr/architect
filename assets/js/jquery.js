f51e5843555646ebc89faf39295dd54dc11b8a12
sakarrr <ctha.sakar@gmail.com>
2024-08-29 11:09:51 +0545

    Refactor - scroll jquery

--- assets/js/jquery.js
+++ assets/js/jquery.js
@@ -3,14 +3,26 @@ $(document).ready(function () {
   $(".arc-menu a").on("click", function (e) {
     e.preventDefault();
 
-    var target = $(this).attr("href");
-    var offset = 54.5;
-
-    $("html, body").animate(
-      {
-        scrollTop: $(target).offset().top - offset,
-      },
-      800
-    );
+    scrollTo($(this));
   });
 });
+
+$(document).ready(function () {
+  $(".arc-branding a").on("click", function (e) {
+    e.preventDefault();
+
+    scrollTo($(this));
+  });
+});
+
+function scrollTo(div) {
+  var target = div.attr("href");
+  var offset = 54.5;
+
+  $("html, body").animate(
+    {
+      scrollTop: $(target).offset().top - offset,
+    },
+    800
+  );
+}
