601df5860303f058979dad1169ab919068a865d3
sakarrr <ctha.sakar@gmail.com>
2024-08-29 15:33:33 +0545

    Add - Func to change header color on scroll

--- assets/js/jquery.js
+++ assets/js/jquery.js
@@ -1,28 +1,38 @@
-// JS to smooth scroll div.
 $(document).ready(function () {
+  // JS to smooth scroll div.
   $(".arc-menu a").on("click", function (e) {
     e.preventDefault();
 
     scrollTo($(this));
   });
-});
 
-$(document).ready(function () {
   $(".arc-branding a").on("click", function (e) {
     e.preventDefault();
 
     scrollTo($(this));
   });
+
+  function scrollTo(div) {
+    var target = div.attr("href");
+    var offset = 54.5;
+
+    $("html, body").animate(
+      {
+        scrollTop: $(target).offset().top - offset,
+      },
+      800
+    );
+  }
+
+  // Change background color on passing certain height.
+  $(window).on("scroll", function () {
+    var scrollHeight = 55;
+    var div = $(".arc-header");
+
+    if ($(window).scrollTop() > scrollHeight) {
+      div.css("background-color", "#fff");
+    } else {
+      div.css("background-color", "transparent");
+    }
+  });
 });
-
-function scrollTo(div) {
-  var target = div.attr("href");
-  var offset = 54.5;
-
-  $("html, body").animate(
-    {
-      scrollTop: $(target).offset().top - offset,
-    },
-    800
-  );
-}
